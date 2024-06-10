f71ce228488283583a5b41033aab585ae9cea2bc
kasheshjaiin <jkashish8055@gmail.com>
2024-06-10 19:48:17 -0400

    pushing widget

--- app.py
+++ app.py
@@ -1,26 +1,39 @@
-from flask import Flask, render_template
-import pandas as pd
+import ipywidgets as widgets
+from IPython.display import display, clear_output
 
-app = Flask(__name__)
+import warnings
+warnings.filterwarnings("ignore")
 
-# Load your dataset
-df = pd.read_csv('ipl_history.csv') 
+venue = widgets.Dropdown(options=df['venue'].unique().tolist(),description='Select Venue:')
+batting_team = widgets.Dropdown(options=df['bat_team'].unique().tolist(),  description='Select Batting Team:')
+bowling_team = widgets.Dropdown(options=df['bowl_team'].unique().tolist(),  description='Select Bowling Team:')
+striker = widgets.Dropdown(options=df['batsman'].unique().tolist(), description='Select Striker:')
+bowler = widgets.Dropdown(options=df['bowler'].unique().tolist(), description='Select Bowler:')
 
-# Function to get unique values for each feature from the dataset
-def get_unique_values(feature):
-    return df[feature].unique()
+predict_button = widgets.Button(description="Predict Score")
 
-# Render the index.html template with dropdown options
-@app.route('/')
-def index():
-    venues = get_unique_values('venue')
-    batting_teams = get_unique_values('bat_team')
-    bowling_teams = get_unique_values('bowl_team')
-    strikers = get_unique_values('batsman')
-    bowlers = get_unique_values('bowler')
-    
-    return render_template('index.html', venues=venues, batting_teams=batting_teams, 
-                           bowling_teams=bowling_teams, strikers=strikers, bowlers=bowlers)
+def predict_score(b):
+    with output:
+        clear_output()  # Clear the previous output
+        
 
-if __name__ == '__main__':
-    app.run(debug=True)
+        # Decode the encoded values back to their original values
+        decoded_venue = venue_encoder.transform([venue.value])
+        decoded_batting_team = batting_team_encoder.transform([batting_team.value])
+        decoded_bowling_team = bowling_team_encoder.transform([bowling_team.value])
+        decoded_striker = batsman_encoder.transform([striker.value])
+        decoded_bowler = bowler_encoder.transform([bowler.value])
+
+
+        input = np.array([decoded_venue,  decoded_batting_team, decoded_bowling_team,decoded_striker, decoded_bowler])
+        input = input.reshape(1,5)
+        input = scaler.transform(input)
+        #print(input)
+        predicted_score = nn_model.predict(input)
+        predicted_score = int(predicted_score[0,0])
+
+        print(predicted_score)
+
+predict_button.on_click(predict_score)
+output = widgets.Output()
+display(venue, batting_team, bowling_team, striker, bowler, predict_button, output)
